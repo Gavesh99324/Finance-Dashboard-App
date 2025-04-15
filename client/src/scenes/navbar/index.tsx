6eba03fa98dbe36e1b3b2c4da407046d5db6f7ab
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-04-15 23:55:40 +0530

    start to create interface

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Typography, useTheme } from '@mui/material';
+import FlexBetween from '@/components/FlexBetween';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
 type Props = {}
 
 const Navbar = (props: Props) => {
 
     const { palette } = useTheme();
+    const [selected, setSelected] = useState("dashboard");
     
   return (
-    <Box display="flex" justifyContent="space-between" alignItems="center">
-    </Box>
+    <FlexBetween mb="o.25rem" p="0.5rem 0rem" color={palette.grey[300]} >
+      <FlexBetween gap="0.75rem">
+        <MonetizationOnIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize={"16px"}>Insightify</Typography>
+      </FlexBetween>
+    </FlexBetween>
   )
 }
 
