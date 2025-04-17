ca7a9beaadbe44c1eef8574de85446ac53eddd76
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-04-17 19:59:15 +0530

    update

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,11 +1,12 @@
 
-import { Box, useTheme } from '@mui/material';
+import DashboardBox from '@/components/DashboardBox';
+import { Box, useTheme, useMediaQuery } from '@mui/material';
 
 type Props = {}
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
   "a b c"
-  "a b c"
+  "a b c"  
   "a b c"
   "a b f"
   "d e f"
@@ -14,20 +15,72 @@ const gridTemplate = `
   "g h i"
   "g h j"
   "g h j"
-`
+`;
 
+const gridTemplateSmallScreens = `
+  "a "
+  "a "  
+  "a "
+  "a "
+  "b "
+  "b "
+  "b "
+  "b "
+  "c "
+  "c "
+  "c "
+  "d "
+  "d "
+  "d "
+  "e "
+  "e "
+  "f "
+  "f "
+  "f "
+  "g "
+  "g "
+  "g "
+  "h "
+  "h "
+  "h "
+  "h "
+  "i "
+  "i "
+  "j "
+  "j "
+`;
+ 
 const Dashboard = (props: Props) => {
+
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
 
     const { palette } = useTheme();
 
   return (
     <Box width={"100%"} display={"grid"} gap={"1.5rem"}
-       sx={{
-        gridTemplateColumns: "repeat(3, minmax(370PX, 1FR))",
-        gridTemplateAreas: gridTemplate, 
-       }}
-    >
-      Dashboard
+       sx={
+        isAboveMediumScreens ? {
+        gridTemplateColumns: "repeat(3, minmax(330PX, 1FR))",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplateLargeScreens, 
+       } : {
+        gridAutoColumns: "1fr",
+        gridAutoRows: "80px",
+        gridTemplateAreas: gridTemplateSmallScreens,
+       }
+      } 
+    > 
+    <DashboardBox />
+      <Box bgcolor={"#fff"} gridArea={"a"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"b"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"c"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"d"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"e"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"f"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"g"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"h"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"i"}></Box>
+      <Box bgcolor={"#fff"} gridArea={"j"}></Box>
     </Box>
   )
 };
