e725bb1c7e1935091f21949ba70e19790265a6cb
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-04-17 17:42:45 +0530

    update

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -15,10 +15,40 @@ const Navbar = (props: Props) => {
     <FlexBetween mb="o.25rem" p="0.5rem 0rem" color={palette.grey[300]} >
       <FlexBetween gap="0.75rem">
         <MonetizationOnIcon sx={{ fontSize: "28px" }} />
-        <Typography variant="h4" fontSize={"16px"}>Insightify</Typography>
+        <Typography variant={"h4"} fontSize={"18px"}>
+          Insightify
+        </Typography>
+      </FlexBetween>
+
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100]}}} >
+          <Link 
+             to={"/"} 
+             onClick={() => setSelected("dashboard")}
+             style={{ 
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit"
+            }}
+             >
+              dashboard
+             </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100]}}} >
+          <Link 
+             to={"/predictions"} 
+             onClick={() => setSelected("predictions")}
+             style={{ 
+              color: selected === "predictions" ? "inherit" : palette.grey[500],
+              textDecoration: "inherit"
+            }}
+             >
+              predictions
+             </Link>
+        </Box>
+        <Box></Box>
       </FlexBetween>
     </FlexBetween>
-  )
-}
+  );
+};
 
 export default Navbar;
