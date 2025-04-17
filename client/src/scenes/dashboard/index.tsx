6689bb56c0cdb93a69d5dc86dabcf80ef66b568f
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-04-17 20:29:11 +0530

    update

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -50,9 +50,10 @@ const gridTemplateSmallScreens = `
   "j "
 `;
  
+
 const Dashboard = (props: Props) => {
 
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1024px)")
 
     const { palette } = useTheme();
 
@@ -60,7 +61,7 @@ const Dashboard = (props: Props) => {
     <Box width={"100%"} display={"grid"} gap={"1.5rem"}
        sx={
         isAboveMediumScreens ? {
-        gridTemplateColumns: "repeat(3, minmax(330PX, 1FR))",
+        gridTemplateColumns: "repeat(3, minmax(330px, 1fr))",
         gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
         gridTemplateAreas: gridTemplateLargeScreens, 
        } : {
@@ -70,17 +71,16 @@ const Dashboard = (props: Props) => {
        }
       } 
     > 
-    <DashboardBox />
-      <Box bgcolor={"#fff"} gridArea={"a"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"b"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"c"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"d"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"e"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"f"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"g"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"h"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"i"}></Box>
-      <Box bgcolor={"#fff"} gridArea={"j"}></Box>
+      <DashboardBox  gridArea={"a"}></DashboardBox>
+      <DashboardBox  gridArea={"b"}></DashboardBox>
+      <DashboardBox  gridArea={"c"}></DashboardBox>
+      <DashboardBox  gridArea={"d"}></DashboardBox>
+      <DashboardBox  gridArea={"e"}></DashboardBox>
+      <DashboardBox  gridArea={"f"}></DashboardBox>
+      <DashboardBox  gridArea={"g"}></DashboardBox>
+      <DashboardBox  gridArea={"h"}></DashboardBox>
+      <DashboardBox  gridArea={"i"}></DashboardBox>
+      <DashboardBox  gridArea={"j"}></DashboardBox>
     </Box>
   )
 };
