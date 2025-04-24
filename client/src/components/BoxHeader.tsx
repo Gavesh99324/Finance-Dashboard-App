d75dbe0fbf40f5e74621ff995f79607ddab4f774
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-04-24 19:09:30 +0530

    finished the operational and non operational chart

--- client/src/components/BoxHeader.tsx
+++ client/src/components/BoxHeader.tsx
@@ -16,11 +16,11 @@ const BoxHeader = ({icon, title, subtitle, sideText}: Props) => {
     const { palette } = useTheme();
 
   return (
-    <FlexBetween color={palette.grey[400]} margin={"1.5rem 1rem 0 1rem"}>
+    <FlexBetween color={palette.grey[100]} margin={"1.5rem 1rem 0 1rem"}>
         <FlexBetween>
             {icon}
             <Box width={"100%"}>
-                <Typography variant='h4' mb={"-0.2rem"}>
+                <Typography variant='h5' mb={"-0.2rem"}>
                     {title}
                 </Typography>
                 <Typography variant='h6'>
--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import DashboardBox from '@/components/DashboardBox';
-import { useGetProductsQuery } from '@/state/api';
-import { BoxHeader } from '@/components/BoxHeader';
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { useGetProductsQuery, useGetKpisQuery } from '@/state/api';
+import BoxHeader  from '@/components/BoxHeader';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { useTheme } from '@mui/material';
-import { revenueProfit } from '@/data';
 
 
 type Props = {}
@@ -12,21 +11,33 @@ type Props = {}
 const Row2 = (props: Props) => {
 
   const { palette } = useTheme();
-  const { data } = useGetProductsQuery();
+  const { data: operationalData } =useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
+  
+  const operationalExpenses = useMemo(() => {
+    return (
+      operationalData &&
+      operationalData[0].monthlyData.map(({ month, operationalExpenses, nonOperationalExpenses }) => {
+        return {
+          name: month.substring(0, 3),
+          "Operational Expenses": operationalExpenses,
+          "Non Operational Expenses": nonOperationalExpenses,
+        };
+      })
+    );
+  }, [operationalData]);
 
   return (
     <>
     <DashboardBox  gridArea={"d"}>
        <BoxHeader 
-         title='Operatonals vs Non-Operatonals Expenses' 
+         title='Operationals vs Non-Operatonals Expenses' 
          subtitle='top line represents revenue, bottom line represents expenses'
          sideText='+4%'
        />
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
-          width={500}
-          height={400}
-          data={revenueProfit}
+          data={operationalExpenses}
           margin={{
             top: 20,
             right: 0,
@@ -36,12 +47,11 @@ const Row2 = (props: Props) => {
         >
           <CartesianGrid vertical={false} stroke={palette.grey[800]} />
           <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
-          <YAxis yAxisId={"left"} tickLine={false} axisLine={false} style={{ fontSize: "10px" }} />
+          <YAxis yAxisId={"left"} orientation='left' tickLine={false} axisLine={false} style={{ fontSize: "10px" }} />
           <YAxis yAxisId={"right"} orientation='right' tickLine={false} axisLine={false} style={{ fontSize: "10px" }} />
           <Tooltip />
-          <Legend height={20} wrapperStyle={{ margin: "0 0 10px 0"}} />
-          <Line yAxisId={"left"} type={"monotone"} dataKey="profit" stroke={palette.tertiary[500]} />
-          <Line yAxisId={"right"} type="monotone" dataKey="revenue" stroke="#f2b455" />
+          <Line yAxisId={"left"} type={"monotone"} dataKey="Non Operational Expenses" stroke={palette.tertiary[500]} />
+          <Line yAxisId={"right"} type="monotone" dataKey="Operational Expenses" stroke={palette.primary.main} />
         </LineChart>
       </ResponsiveContainer>
     </DashboardBox>
